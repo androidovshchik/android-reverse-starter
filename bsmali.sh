32afb7a1a02b502f48712c9941105cfe9c3a814a
vlad <vladkalyuzhnyu@gmail.com>
2019-08-07 21:09:02 +0300

    changes

--- bsmali.sh
+++ bsmali.sh
@@ -4,3 +4,11 @@ if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
     echo "Usage: `basename $0` [optional args]"
     exit 0
 fi
+
+apktool_version=$(jq -r '.apktool_version' config.json)
+
+dirs=($(ls -d smali_* 2> /dev/null | sort -r))
+if [[ -n "$dirs" ]]; then
+    dir=${dirs[0]}
+    java -jar _libs/apktool_${apktool_version}.jar b ${dir} $@
+fi
--- djadx.sh
+++ djadx.sh
@@ -9,7 +9,7 @@ jadx_version=$(jq -r '.jadx_version' config.json)
 
 IFS=$'\n'
 for apk in $(ls *.apk 2> /dev/null); do
-    dir=jadx_$(date '+%Y%m%d_%H%M%S')_$(echo "${apk}" | sed -r 's/[^a-zA-Z0-9]+//g')
+    dir=jadx_$(date '+%Y%m%d_%H%M%S')_$(echo "${apk}" | sed -r 's/([^a-zA-Z0-9]+|.apk$)//g')
     _jadx-${jadx_version}/bin/jadx "${apk}" $@ -d ${dir} | grep -vE "(ERROR|WARN|JADX INFO|Debug info|^\s)"
     sleep 1
 done
