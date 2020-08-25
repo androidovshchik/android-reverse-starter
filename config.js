ea8f720d59575c224ee047d343dfb63b11c46026
vlad <vladkalyuzhnyu@gmail.com>
2020-08-25 18:45:21 +0300

    nodejs replacement

---
+++ config.js
@@ -0,0 +1,7 @@
+// noinspection SpellCheckingInspection
+module.exports = Object.freeze({
+    uber_apk_signer_version: '1.1.0',
+    apktool_version: '2.4.1',
+    jadx_version: '1.1.0',
+    ANDROID_HOME: '/home/vlad/android-sdk',
+});
